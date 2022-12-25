2d5a450a7c735da2ad475b1c54db44a9f925789a
kanji4242 <kanji@free.fr>
2022-12-25 01:49:57 +0100

    day8 done

--- day8/day8.py
+++ day8/day8.py
@@ -36,20 +36,17 @@ def scan_visible(line_or_col, visible_datas):
 
 
 def get_scenic_score(line_or_col, index):
-    print(line_or_col, index, line_or_col[index])
-    max_height_left = max_height_right = -1
     view_left = view_right = 0
     for i in range(index - 1, -1, -1):
-        if line_or_col[i] >= max_height_left and line_or_col[i] <= line_or_col[index]:
-            max_height_left = line_or_col[i]
-            view_left += 1
+        view_left += 1
+        if line_or_col[i] >= line_or_col[index]:
+            break
 
     for i in range(index + 1, len(line_or_col)):
-        if line_or_col[i] >= max_height_right and line_or_col[i] <= line_or_col[index]:
-            max_height_right = line_or_col[i]
-            view_right += 1
+        view_right += 1
+        if line_or_col[i] >= line_or_col[index]:
+            break
 
-    print(view_left, view_right)
     return view_left * view_right
 
 
@@ -59,8 +56,6 @@ def day8_1(file):
     for n in range(grid.shape[0]):
         scan_visible(grid[n], visible_trees[n])
         scan_visible(grid[:, n], visible_trees[:, n])
-    print(grid)
-    print(visible_trees)
     print(np.count_nonzero(visible_trees == 1))
 
 
@@ -70,13 +65,12 @@ def day8_2(file):
     for x in range(grid.shape[0]):
         for y in range(grid.shape[0]):
             score = get_scenic_score(grid[:, x], y) * get_scenic_score(grid[y], x)
-            print("---", x, y, score)
             scenic_scores.append(score)
     print(max(scenic_scores))
 
 
 
 if __name__ == '__main__':
-    #day8_1(sys.argv[1])
+    day8_1(sys.argv[1])
     day8_2(sys.argv[1])
 
