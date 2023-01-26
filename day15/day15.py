e2df3665bb548ec275b254f95aaa6574a91ac4b8
kanji4242 <kanji@free.fr>
2023-01-27 00:00:33 +0100

    day15 done ! 27*

--- day15/day15.py
+++ day15/day15.py
@@ -6,83 +6,109 @@
 
 import sys
 import re
-import numpy as np
-
-CELL_VOID = 0
-CELL_BEACON = 1
-CELL_SENSOR = 2
-CELL_COVER = 3
 
 
-def symbol(cell):
-    cell_map = {CELL_VOID: '.', CELL_BEACON: 'B', CELL_SENSOR: 'S', CELL_COVER: '#'}
-    return cell_map[cell]
+def read_sensors(file):
+    sensors = []
+    with open(file) as f:
+        for line in f:
+            if m := re.match(r'Sensor at x=([-\d]+), y=([-\d]+): closest beacon is at x=([-\d]+), y=([-\d]+)',
+                             line.strip()):
+                sensors.append(tuple([int(x) for x in list(m.groups())]))
+
+    return sensors
 
 
-def display_grid(grid):
-    liney = 0
-    for y in range(grid.shape[1]):
-        print(''.join([symbol(grid[x][y]) for x in range(grid.shape[0])]), " <-" if liney == 10 else "")
-        liney += 1
-
-
-def manhattan_distance(s):
+def get_manhattan_distance(s):
     return abs(s[1] - s[3]) + abs(s[0] - s[2])
 
 
+def add_sensor_edges(sensor):
+    points = []
+    manhattan_distance = get_manhattan_distance(sensor) + 1
+
+    points.append((sensor[0] - manhattan_distance, sensor[1]))
+    points.append((sensor[0] + manhattan_distance, sensor[1]))
+    for md in range(- manhattan_distance + 1, manhattan_distance):
+        points.append((sensor[0] + md, sensor[1] + abs(md) - manhattan_distance))
+        points.append((sensor[0] + md, sensor[1] - abs(md) + manhattan_distance))
+
+    return points
+
+
+def is_covered(point, sensors):
+    covered = False
+    for s in sensors:
+        manhattan_distance = get_manhattan_distance(s)
+        if (abs(point[0] - s[0]) + abs(point[1] - s[1])) <= manhattan_distance:
+            covered = True
+            break
+    return covered
+
+
+def get_sensors_boundary(sensors, with_md=False):
+    if with_md:
+        return (min([s[0] - get_manhattan_distance(s) for s in sensors]),
+                max([s[0] + get_manhattan_distance(s) for s in sensors]),
+                min([s[1] - get_manhattan_distance(s) for s in sensors]),
+                max([s[1] + get_manhattan_distance(s) for s in sensors]))
+    else:
+        return (min([s[0] for s in sensors]),
+                max([s[0] for s in sensors]),
+                min([s[1] for s in sensors]),
+                max([s[1] for s in sensors]))
+
+
+def clean_edges(points, sensors):
+    return list(filter(lambda p: not is_covered(p, sensors), points))
+
+
+def crop_area(points, boundary):
+    return list(filter(
+        lambda p: (boundary[0] <= p[0] <= boundary[1]) and (boundary[2] <= p[1] <= boundary[3]),
+        points))
+
+
 def day15_1(file, line_no=10):
-    sensors = []
-    with open(file) as f:
-        for line in f:
-            if m := re.match(r'Sensor at x=([-\d]+), y=([-\d]+): closest beacon is at x=([-\d]+), y=([-\d]+)',
-                             line.strip()):
-                sensors.append(tuple([int(x) for x in list(m.groups())]))
+    sensors = read_sensors(file)
+    boundary = get_sensors_boundary(sensors, with_md=True)
+    print("Sensors boundary with manhattan distance:", boundary)
 
-    min_x = min([s[0] - manhattan_distance(s) for s in sensors])
-    max_x = max([s[0] + manhattan_distance(s) for s in sensors])
-    size = max_x - min_x
-    row = np.zeros((size,))
+    beacons = list(set([(s[2], s[3]) for s in sensors]))
+    print("Unique beacon:", len(beacons))
+    beacons_line_no = len(list(filter(lambda b: b[1] == line_no, beacons)))
+    print(f"Unique beacon at line {line_no}:", beacons_line_no)
 
-    for s in sensors:
-        if s[1] == line_no:
-            row[s[0] - min_x] = CELL_SENSOR
-        if s[3] == line_no:
-            row[s[2] - min_x] = CELL_BEACON
-
-    for s in sensors:
-        md = manhattan_distance(s)
-        if s[1] - md <= line_no <= s[1] + md:
-            sensor_to_line_no = abs(line_no - s[1])
-            for x in range(s[0] - (md - sensor_to_line_no) - min_x,
-                           s[0] + (md - sensor_to_line_no) - min_x + 1):
-                if 0 <= x < row.shape[0]:
-                    if row[x] == CELL_VOID:
-                        row[x] = CELL_COVER
-
-    #print(''.join([symbol(x) for x in row[-min_x:20-min_x+1]]))
-    print(len([x for x in row if x == CELL_COVER]))
+    cell_covered = len(list(filter(lambda x: is_covered((x, line_no), sensors),
+                                   list(range(boundary[0], boundary[1] + 1)))))
+    print(cell_covered - beacons_line_no)
 
 
 def day15_2(file):
-    sensors = []
-    with open(file) as f:
-        for line in f:
-            if m := re.match(r'Sensor at x=([-\d]+), y=([-\d]+): closest beacon is at x=([-\d]+), y=([-\d]+)',
-                             line.strip()):
-                sensors.append(tuple([int(x) for x in list(m.groups())]))
+    sensors = read_sensors(file)
+    boundary = get_sensors_boundary(sensors)
+    print("Sensors boundary:", boundary)
 
-        print(sum([(manhattan_distance(s)+2)*4 for s in sensors]))
-        min_x = min([s[0] - manhattan_distance(s) for s in sensors])
-        max_x = max([s[0] + manhattan_distance(s) for s in sensors])
-        size_x = max_x - min_x
-        min_y = min([s[1] - manhattan_distance(s) for s in sensors])
-        max_y = max([s[1] + manhattan_distance(s) for s in sensors])
-        size_y = max_y - min_y
-        #print(sum([manhattan_distance(s) for s in sensors]))
-        #print(size_x * size_y)
+    points = []
+    for i in range(len(sensors)):
+        s = sensors[i]
+        print("Processing sensor:", s, "manhattan distance: ", get_manhattan_distance(s),
+              f"(+{(get_manhattan_distance(s) + 1) * 4})")
+        points.extend(add_sensor_edges(s))
+        print("Points nb:", len(points))
+        print("")
+
+    pe = len(points)
+    points = clean_edges(points, sensors[:i + 1])
+    print("Cleaned Points nb:", len(points), f"({len(points) - pe})")
+    points = crop_area(points, boundary)
+    print("Cropped Points nb:", len(points), len(set(points)))
+
+    tuning_freq = list(set(points))[0][0] * 4_000_000 + list(set(points))[0][1]
+    print(tuning_freq)
 
 
 if __name__ == '__main__':
     day15_1(sys.argv[1], line_no=2_000_000)
-    #day15_2(sys.argv[1])
+    day15_2(sys.argv[1])
 
