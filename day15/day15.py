29223899719ef51e81c099b0f4f4d41905d08838
kanji4242 <kanji@free.fr>
2023-02-10 01:22:12 +0100

    Add comments

--- day15/day15.py
+++ day15/day15.py
@@ -9,6 +9,7 @@ import re
 
 
 def read_sensors(file):
+    # REad all sensors information and insert into in a list
     sensors = []
     with open(file) as f:
         for line in f:
@@ -20,10 +21,28 @@ def read_sensors(file):
 
 
 def get_manhattan_distance(s):
+    # Find the Manhattan distance between the sensor and its beacon
+    # This is simply the sum of the absolute values of the differences in both coordinates.
     return abs(s[1] - s[3]) + abs(s[0] - s[2])
 
 
 def add_sensor_edges(sensor):
+    # Returns all point coordinate that below to the edge of a sensor area with its beacon.
+    # The edge of a sensor area are all point that are a direct neighbor of the area boundary
+    # For instance, a sensor located at [4, 4] and its beacon located at [2, 6], the method will return
+    # the following points coordinates marked as "E" ("S" is the sensor, "B" is the beacon):
+    #      0    5
+    #    ......E.....
+    #  0 .....E.E....
+    #    ....E...E...
+    #    ...E.....E..
+    #    ..E.......E.
+    #    .E....S....E
+    #  5 ..E.......E.
+    #    ...EB....E..
+    #    ....E...E...
+    #    .....E.E....
+    #    ......E.....
     points = []
     manhattan_distance = get_manhattan_distance(sensor) + 1
 
@@ -37,6 +56,26 @@ def add_sensor_edges(sensor):
 
 
 def is_covered(point, sensors):
+    # Determine if a point is within the area covered of at least one sensor given in the list
+    # The area of sensor depend on its beacon and their Manhattan distance and has a rhombus shape.
+    # For instance, a sensor located at [4, 4] and its beacon located at [2, 6] have a Manhattan distance of 4
+    # Their area will look like a rhombus centered at [4, 4] with all its 4 edges locate 4 units away.
+    # Every point marked "#" has a Manhattan distance less or equal to 4 with the sensor.
+    # Given this area, a point X at [0, 2] will be outside the area, while a point Y at [5, 6] will be inside.
+    # ("S" is the sensor, "B" is the beacon, "X" and "Y" are the 2 points mentioned above)
+    #     0   4   8
+    #  0 .....#....
+    #    ....###...
+    #    .X.#####..
+    #    ..#######.
+    #  4 .####S####
+    #    ..#######.
+    #    ...B##Y#..
+    #    ....###...
+    #  8 .....#....
+    #
+    # This check is performed for every sensor provided in the list. If the point is found inside a sensor area
+    # the method returns True
     covered = False
     for s in sensors:
         manhattan_distance = get_manhattan_distance(s)
@@ -47,6 +86,8 @@ def is_covered(point, sensors):
 
 
 def get_sensors_boundary(sensors, with_md=False):
+    # Determine the minimum and the maximum coordinates for a list sensors
+    # if with_md is defined, consider the area covered with its beacon and using its Manhattan distance
     if with_md:
         return (min([s[0] - get_manhattan_distance(s) for s in sensors]),
                 max([s[0] + get_manhattan_distance(s) for s in sensors]),
@@ -71,16 +112,23 @@ def crop_area(points, boundary):
 
 def day15_1(file, line_no=10):
     sensors = read_sensors(file)
+    # Find the boundaries of all sensors including their area. With this information we have a range
+    # of coordinates to work with
     boundary = get_sensors_boundary(sensors, with_md=True)
     print("Sensors boundary with manhattan distance:", boundary)
 
+    # Get the beacon coordinates to delete them because beacons do not count on the result
     beacons = list(set([(s[2], s[3]) for s in sensors]))
     print("Unique beacon:", len(beacons))
+    # Find the number of beacon located on the considered line
     beacons_line_no = len(list(filter(lambda b: b[1] == line_no, beacons)))
     print(f"Unique beacon at line {line_no}:", beacons_line_no)
 
+    # Get the number of points covered by all sensors
     cell_covered = len(list(filter(lambda x: is_covered((x, line_no), sensors),
                                    list(range(boundary[0], boundary[1] + 1)))))
+
+    # Returns this number without the number of beacon found previously
     print(cell_covered - beacons_line_no)
 
 
@@ -89,6 +137,18 @@ def day15_2(file):
     boundary = get_sensors_boundary(sensors)
     print("Sensors boundary:", boundary)
 
+    # The part2 is trickier, we need to find a single point not covered by any sensor.
+    # The problem is that the grid is far too big, we cannot work with a numpy array because it would require
+    # several EiB of RAM memory to store it. So we need to find another way.
+    # We can notice that this point is a single point, so it must lie on an edge of several sensors.
+    # So the idea is to focus on the edges point of an area instead of the area points themselves, this approach
+    # will drastically reduce the memory needed.
+    # We place in a list the edge points of the first sensor, append to the list the edge points of the second
+    # sensor, do the same for third one, and so on and so forth.
+    # Then delete the edge points we have in the list covered by any of all sensors.
+    # We also filter (crop) the points, because by definition the point cannot lie belong any sensor location.
+    # And at the end, we should have few points with the same coordinate because this single point belong to edge
+    # of several sensors. And this point is the answer.
     points = []
     for i in range(len(sensors)):
         s = sensors[i]
