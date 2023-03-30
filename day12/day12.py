40b5a26ca454a6d68f938a266a3e225e95bb871c
kanji4242 <kanji@free.fr>
2023-03-30 08:01:55 +0200

    day12 done! 50* Advent of Code 2022 finished !

--- day12/day12.py
+++ day12/day12.py
@@ -4,104 +4,160 @@
 # https://adventofcode.com/2022/day/12
 #
 
+"""
+The idea is to parse the input heightmap, convert each height which are letter to a numerical value. My convention is:
+ - the current position has a value of 0 (although the current position is stated to have an elevation a, it does not
+   change the result)
+ - any heights from a to z have a value respectively from 1 to 26
+ - the end position has a value of 0 (same as current position, although it is assumed to have an elevation z)
+
+The heightmap is to put in a numpy array with the numerical values mentioned above.
+We use the Dijkstra's algorithm in both parts but in different ways
+
+For port 1:
+We build a graph, by parsing all positions from the numpy array. For every position, we add a new node on the
+graph, and then look at the neighbour positions up, down, left, and right, and set a vertex for every position that
+has a height from 1 up to at most one higher than the current position.
+We run the Dijkstra's algorithm with this graph from the current position, and just return the distance to the
+end position.
+
+For port 2:
+We proceed in the same way, but instead we look at the neighbour positions in a descending way. That is, we set
+a vertex for every position that has a height at least one lower than the current position.
+We run the Dijkstra's algorithm with this graph from the end position, and return the shortest distance found from
+all positions which has a height a (or 1 numerically).
+
+"""
+
 import sys
 import numpy as np
-from queue import Queue
 
 
-def level(ch):
+def chr2level(ch):
+    # Convert each height letter to a numerical value, like mentioned above.
     if ch == 'S':
-        ch = "a"
+        return 0
     elif ch == 'E':
-        ch = "z"
-    return ord(ch) - ord('a')
+        return 27
+    else:
+        return ord(ch) - ord('a') + 1
 
 
-def find_shortest_path(heightmap, start, end):
-    queue = Queue()
-    queue.put(start)
+def dijkstra(graph, start):
+    # The Dijkstra's algorithm
 
-    path = {start: 0}
-    pathmap = np.chararray(heightmap.shape, itemsize=3)
-    pathmap[:] = "."
+    # Initialize
+    Q = set(graph.keys())
+    dist = {node: float('infinity') for node in Q}
+    dist[start] = 0
 
-    pathmap[start[0]][start[1]] = 0
+    while Q:
+        # Select closest node
+        u = min(Q, key=lambda node: dist[node])
+        Q.remove(u)
 
-    while not queue.empty():
-        current = queue.get()
-        #print("get", current, end)
+        # Update distances of neighbour nodes
+        for v in graph[u]:
+            if dist[v] > dist[u] + 1:
+                dist[v] = dist[u] + 1
 
-        if current == end:
-            return max(list(path.values()))
-
-        for neighbor in [
-            (current[0] + 1, current[1]),
-            (current[0] - 1, current[1]),
-            (current[0], current[1] + 1),
-            (current[0], current[1] - 1)
-        ]:
-            if 0 <= neighbor[0] < heightmap.shape[0] and 0 <= neighbor[1] < heightmap.shape[1]:
-                #print("inspect", path[current], neighbor, heightmap[neighbor[0]][neighbor[1]], heightmap[current[0]][current[1]])
-                #print_array_bytes(pathmap)
-                if (neighbor not in path or path[neighbor] > path[current] + 1) \
-                        and heightmap[current[0]][current[1]] <= heightmap[neighbor[0]][neighbor[1]] <= heightmap[current[0]][current[1]] + 1:
-                    queue.put(neighbor)
-                    #print("put", neighbor)
-                    path[neighbor] = path[current] + 1
-                    pathmap[neighbor[0]][neighbor[1]] = str(path[current] + 1)
-
-    print_array_all(heightmap, pathmap)
-    return "No path found"
+    return dist
 
 
-def print_array_int(array):
-    for x in range(array.shape[0]):
-        print(" ".join([f"{y:02}" for y in array[x]]))
+def find_best_distance_to_end(grid, start, end):
+    # Initialize a dict graph
+    graph = {}
+
+    for x in range(grid.shape[0]):
+        for y in range(grid.shape[1]):
+            # Always create a new node for each position
+            graph[(x, y)] = []
+            for neighbor in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
+                # The neighbor positions must be i the boundary of the array
+                if 0 <= neighbor[0] < grid.shape[0] and 0 <= neighbor[1] < grid.shape[1]:
+                    # Set a vertex for every position that has a height from 1 up to at most one higher than
+                    # the current position.
+                    if 1 <= grid[neighbor[0]][neighbor[1]] <= grid[x, y] + 1:
+                        graph[(x, y)].append((neighbor[0], neighbor[1]))
+
+    # Run the Dijkstra's algorithm from the current position and return the distance to the end position.
+    dist = dijkstra(graph, start)
+    return dist[end]
 
 
-def print_array_bytes(array):
-    for x in range(array.shape[0]):
-        print(" ".join([f"{y.decode('utf-8'):2}" for y in array[x]]))
+def get_best_distance_from_a(grid, end):
+    # Initialize a dict graph
+    graph = {}
 
-def print_array_all(array_map, array_path):
-    print(array_map.shape, array_path.shape)
-    for x in range(array_map.shape[0]):
-        print(" ".join([f"{y[0]:02}-{y[1].decode('utf-8'):3}" for y in zip(array_map[x][41:74], array_path[x][41:74])]))
+    for x in range(grid.shape[0]):
+        for y in range(grid.shape[1]):
+            # Always create a new node for each position
+            # To keep track of the height on the nodes, we also add the height in the node name. This will be useful
+            # later to find the best distance
+            graph[(x, y, grid[x, y])] = []
+            for neighbor in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
+                # The neighbor positions must be i the boundary of the array
+                if 0 <= neighbor[0] < grid.shape[0] and 0 <= neighbor[1] < grid.shape[1]:
+                    # Set a vertex for every position that has a height at least one lower than the current position
+                    if grid[neighbor[0]][neighbor[1]] >= grid[x, y] - 1:
+                        graph[(x, y, grid[x, y])].append((neighbor[0], neighbor[1], grid[neighbor[0], neighbor[1]]))
+
+    # Run the Dijkstra's algorithm from the end position and return the shortest distance found from
+    # all positions which has a height a
+    dist = dijkstra(graph, end)
+    return min([dist[d] for d in dist if d[2] == chr2level('a')])
 
 
+def parse_heightmap(file):
+    # First parse to find the size of the heightmap
+    with open(file) as f:
+        size_x, size_y = 0, 0
 
-def day12_1(file):
-    grid = None
+        for line in f:
+            size_x = max(size_x, len(line.rstrip()))
+            size_y += 1
+
+    # Initialize the array
+    grid = np.ndarray((size_x, size_y), dtype=int)
     start_coord = None
     end_coord = None
 
+    # Second parse to fill the array
     with open(file) as f:
-        n = 0
+        y = 0
         for line in f:
             line = line.rstrip()
-            if grid is None:
-                grid = np.array(list([level(x) for x in line]), dtype=int)
-            else:
-                grid = np.vstack((grid, np.array(list([level(x) for x in line]))))
+            grid[:, y] = [chr2level(x) for x in line]
+
+            # Note the current and end position
             if line.find("S") >= 0:
-                start_coord = (n, line.find("S"))
+                start_coord = (line.find('S'), y)
             if line.find("E") >= 0:
-                end_coord = (n, line.find("E"))
-            n += 1
+                end_coord = (line.find('E'), y)
+            y += 1
 
-    print(start_coord, end_coord)
-    print_array_int(grid)
-    print(find_shortest_path(grid, start_coord, end_coord))
+    return grid, start_coord, end_coord
 
+
+def display_heightmap(grid):
+    # Display the heightmap (for debugging purpose only)
+    for y in range(grid.shape[1]):
+        print(" ".join([f"{grid[x, y]:02}" for x in range(grid.shape[0])]))
+
+
+def day12_1(file):
+    grid, start_coord, end_coord = parse_heightmap(file)
+    print(find_best_distance_to_end(grid, start_coord, end_coord))
 
 
 def day12_2(file):
-    with open(file) as f:
-        for line in f:
-            pass
+    grid, start_coord, end_coord = parse_heightmap(file)
+    # We keep track of the height on the nodes
+    end_coord = (end_coord[0], end_coord[1], chr2level('E'))
+    print(get_best_distance_from_a(grid, end_coord))
 
 
 if __name__ == '__main__':
     day12_1(sys.argv[1])
-    #day12_2(sys.argv[1])
+    day12_2(sys.argv[1])
 
