d5a569b11decde958714473271a75903879f609c
kanji4242 <kanji@free.fr>
2023-12-05 22:46:25 +0100

    Fix code for part2. 8*

--- 2023/day1/day1.py
+++ 2023/day1/day1.py
@@ -7,9 +7,30 @@
 import sys
 
 
-def scan_document(file, with_letters_digits=False):
+def convert_letters_digits(line):
+    # If you look at the lines you can see that some numbers overlap by one character. My trick is to replace
+    # each number spelt out by the number written in figures but surrounded by its first and last characters.
+    letters_digits = {
+        'one': "o1e",
+        'two': "t2o",
+        'three': "t3e",
+        'four': "f4r",
+        'five': "f5e",
+        'six': "s6x",
+        'seven': "s7n",
+        'eight': "e8t",
+        'nine': "n9e"
+    }
+    new_line = line
+
+    for letters_digit, replacement in letters_digits.items():
+        new_line = new_line.replace(letters_digit, replacement)
+
+    return new_line
+
+
+def find_calibration_sum(file, with_letters_digits=False):
     calibration_sum = 0
-    letters_digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
     with open(file) as f:
         for line in f:
@@ -17,52 +38,28 @@ def scan_document(file, with_letters_digits=False):
             first_digit, last_digit = None, None
 
             if with_letters_digits:
-                new_line = ""
-                for ch in line:
-                    new_line = new_line + ch
-                    for digit, letters_digit in enumerate(letters_digits):
-                        new_line = new_line.replace(letters_digit, f"{digit + 1}")
-
-                new_line2 = line
-                for digit, letters_digit in enumerate(letters_digits):
-                    new_line2 = new_line2.replace(letters_digit, f"{digit + 1}")
-
-                new_line3 = line
-                i = 0
-                while i < len(new_line3):
-                    for digit, letters_digit in enumerate(letters_digits):
-                        #print(i, new_line3[i:], letters_digit)
-                        if new_line3[i:].startswith(letters_digit):
-                            new_line3 = new_line3[:i] + f"{digit + 1}" + new_line3[i + len(letters_digit):]
-                    i += 1
-
-                print(line, new_line3)
-
-                #if new_line != new_line2:
-                #    print(line, new_line, new_line2)
-                line = new_line3
+                line = convert_letters_digits(line)
 
             for ch in line:
-                if ord('0') <= ord(ch) <= ord('9'):
+                if ord('0') < ord(ch) <= ord('9'):
                     if not first_digit:
                         first_digit = ch
                     last_digit = ch
 
-            print(first_digit, last_digit)
-
             calibration_sum += int(first_digit + last_digit)
 
-    print(calibration_sum)
+    return calibration_sum
 
 
 def day1_1(file):
-    scan_document(file)
+    print(find_calibration_sum(file))
+
 
 def day1_2(file):
-    scan_document(file, with_letters_digits=True)
+    print(find_calibration_sum(file, with_letters_digits=True))
 
 
 if __name__ == '__main__':
-    #day1_1(sys.argv[1])
+    day1_1(sys.argv[1])
     day1_2(sys.argv[1])
 
